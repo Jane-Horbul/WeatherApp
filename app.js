5c423b0cb1b60071fc6bf3f000ca6233f3588619
Jane-Horbul <es.horbul@gmail.com>
2021-09-16 22:28:30 +0300

    Added animated icon

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
+const weatherMap = {
+    "Clear": "CLEAR_DAY",
+    "Clear-night": "CLEAR_NIGHT",
+    "Partly-cloudy-day": "PARTLY_CLOUDY_DAY",
+    "Partly-cloudy-night": "PARTLY_CLOUDY_NIGHT",
+    "Clouds": "CLOUDY",
+    "Rain": "RAIN",
+    "Sleet": "SLEET",
+    "Snow": "SNOW",
+    "Wind": "WIND",
+    "Fog": "FOG",
+};
+
+function setIcon(data, iconID) {
+        const skycons = new Skycons({color:"white"});
+        const currentIcon = data.weather[0].main;
+        skycons.play();
+        return skycons.set(iconID, Skycons[weatherMap[currentIcon]]);
+    }
+
+
 window.addEventListener('load', ()=> {
     let long;
     let lat;
     let locationTimezone = document.querySelector(".location-timezone");
     let temperatureDegree = document.querySelector('.temperature-degree');
     let temperatureDiscription = document.querySelector(".temperature-discription");
+    let icon;
 
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -18,16 +40,19 @@ window.addEventListener('load', ()=> {
             return response.json();
         })
         .then(data => {
-            //console.log(data);
-            temperatureDegree.textContent = data.main.temp;
-            //console.log (temperatureDegree);
-            locationTimezone.textContent = data.name
-            //console.log(locationTimezone);
-            temperatureDiscription.textContent = data.weather[0].description;
-         });
-     });
 
+            temperatureDegree.textContent = data.main.temp;
+            locationTimezone.textContent = data.name;
+            temperatureDiscription.textContent = data.weather[0].description;
+            
+
+            setIcon(data, document.querySelector('.icon'));
+        });
+     });
     }
+
+    
+    
 });
 
 
